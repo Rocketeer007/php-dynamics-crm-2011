5fd5bb9cd76cca5f6eca1819e60094cc6f359524
N.M.Price@gmail.com <N.M.Price@gmail.com@c2f3a525-6e19-95a0-aabe-37e2636e8d83>
2012-03-16 16:21:57 +0000

    FIXED - task 14: Implement an automatic class loader http://code.google.com/p/php-dynamics-crm-2011/issues/detail?id=14

--- DynamicsCRM2011.php
+++ DynamicsCRM2011.php
@@ -13,9 +13,36 @@ abstract class DynamicsCRM2011 implements DynamicsCRM2011_Interface {
 	protected static function stripNS($attributeValue) {
 		return preg_replace('/[a-zA-Z]+:([a-zA-Z]+)/', '$1', $attributeValue);
 	}
+	
+	/**
+	 * Implementation of Class Autoloader
+	 * See http://www.php.net/manual/en/function.spl-autoload-register.php
+	 * 
+	 * @param String $className the name of the Class to load
+	 */
+	public static function loadClass($className){
+		/* Only load classes that don't exist, and are part of DynamicsCRM2011 */
+		if ((class_exists($className)) || (strpos($className, 'DynamicsCRM2011') === false)) {
+			return false;
+		}
+
+		/* Work out the filename of the Class to be loaded.
+		 * NOTE: If this ever moves to a directory structure, we will need to 
+		 * compensate for relative paths etc.
+		 */
+		$classFilePath = $className.'.class.php';
+
+		/* Only try to load files that actually exist and can be read */
+		if ((file_exists($classFilePath) === false) || (is_readable($classFilePath) === false)) {
+			return false;
+		}
+
+		/* Don't load it if it's already been loaded */
+		require_once $classFilePath;
+	}
 }
 
-require_once 'DynamicsCRM2011_Connector.class.php';
-require_once 'DynamicsCRM2011_Entity.class.php';
-require_once 'DynamicsCRM2011_Incident.class.php';
-require_once 'DynamicsCRM2011_Account.class.php';
+/* Register the Class Loader */
+spl_autoload_register(Array('DynamicsCRM2011', 'loadClass'));
+
+?>
