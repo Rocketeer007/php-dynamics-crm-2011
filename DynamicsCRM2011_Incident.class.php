82f43ee2e7d4a9125d90cb51eca537ea57a91bd7
N.M.Price@gmail.com <N.M.Price@gmail.com@c2f3a525-6e19-95a0-aabe-37e2636e8d83>
2012-03-20 11:57:06 +0000

    BUGFIX: Cannot set values due to incorrect checking of property type ENHANCEMENT: Added "DisplayName" usage, to allow setting of generic fields to display on Entity details

--- DynamicsCRM2011_Incident.class.php
+++ DynamicsCRM2011_Incident.class.php
@@ -4,6 +4,7 @@ require_once 'DynamicsCRM2011.php';
 
 class DynamicsCRM2011_Incident extends DynamicsCRM2011_Entity {
 	protected $entityLogicalName = 'incident'; 
+	protected $entityDisplayName = 'ticketnumber';
 	
 	public function __toString() {
 		$description = 'Incident: '.$this->TicketNumber.' <'.$this->ID.'>';
