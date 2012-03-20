f418d159cf9b1f4ed5a7d16a86e0d7713c2b18e3
N.M.Price@gmail.com <N.M.Price@gmail.com@c2f3a525-6e19-95a0-aabe-37e2636e8d83>
2012-03-20 11:04:12 +0000

    Removed un-needed Includes, added pretty __toString function

--- trunk/DynamicsCRM2011_Contact.class.php
+++ trunk/DynamicsCRM2011_Contact.class.php
@@ -2,9 +2,9 @@
 
 require_once 'DynamicsCRM2011.php';
 
-class DynamicsCRM2011_Account extends DynamicsCRM2011_Entity {
-	protected $entityLogicalName = 'account';
-
+class DynamicsCRM2011_Contact extends DynamicsCRM2011_Entity {
+	protected $entityLogicalName = 'contact'; 
+	
 	public function __toString() {
 		$description = 'Contact: '.$this->Name.' <'.$this->ID.'>';
 		return $description;
--- trunk/DynamicsCRM2011_Incident.class.php
+++ trunk/DynamicsCRM2011_Incident.class.php
@@ -1,10 +1,14 @@
 <?php
 
-require_once 'DynamicsCRM2011_Connector.class.php';
-require_once 'DynamicsCRM2011_Entity.class.php';
+require_once 'DynamicsCRM2011.php';
 
 class DynamicsCRM2011_Incident extends DynamicsCRM2011_Entity {
 	protected $entityLogicalName = 'incident'; 
+	
+	public function __toString() {
+		$description = 'Incident: '.$this->TicketNumber.' <'.$this->ID.'>';
+		return $description;
+	}
 }
 
 ?>
