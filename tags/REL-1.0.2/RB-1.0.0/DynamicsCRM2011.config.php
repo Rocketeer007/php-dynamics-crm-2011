0162ed636dda59bc98d3d1de6c77e7bbfdbe86a1
N.M.Price@gmail.com <N.M.Price@gmail.com@c2f3a525-6e19-95a0-aabe-37e2636e8d83>
2012-04-16 09:22:57 +0000

    Tag release 1.0.2

---
+++ tags/REL-1.0.2/RB-1.0.0/DynamicsCRM2011.config.php
@@ -0,0 +1,14 @@
+<?php
+/* Basic configuation details */
+// Discovery Service URI -> Get this from the Customizations / Developer Resources section of the Microsoft Dynamics CRM 2011 application
+$discoveryServiceURI = 'https://<your.domain.name>/XRMServices/2011/Discovery.svc';
+// Organization Unique Name -> Get this from the Customizations / Developer Resources section of the Microsoft Dynamics CRM 2011 application
+$organizationUniqueName = '<YourOrganizationName>';
+// Username & Password - Note that despite the text on the ADFS site, the Domain is NOT required!
+$loginUsername = '<User Name>';
+$loginPassword = '<Password>';
+
+/* Example data used in the Demos */
+$demoCaseNumber = 'CAS-XXXXX-XXXX';
+$demoContactEmail = 'user@example.com';
+?>
