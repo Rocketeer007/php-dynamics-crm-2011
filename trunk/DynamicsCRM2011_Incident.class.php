9b01a1fe878998faabf8d38003487cade416a8d3
N.M.Price@gmail.com <N.M.Price@gmail.com@c2f3a525-6e19-95a0-aabe-37e2636e8d83>
2014-03-12 22:17:25 +0000

    Support a CloseIncidentRequest

--- trunk/DynamicsCRM2011_Incident.class.php
+++ trunk/DynamicsCRM2011_Incident.class.php
@@ -10,6 +10,21 @@ class DynamicsCRM2011_Incident extends DynamicsCRM2011_Entity {
 		$description = 'Incident: '.$this->TicketNumber.' <'.$this->ID.'>';
 		return $description;
 	}
+	
+	public function close(DynamicsCRM2011_Connector $crmConnector, $status, $subject = NULL) {
+		if ($this->ID == self::EmptyGUID) {
+			throw new Exception('Cannot Close an Incident that has not been Created!');
+			return FALSE;
+		}
+		/* Create the Incident Resolution */
+		$_resolution = new DynamicsCRM2011_IncidentResolution($crmConnector);
+		/* Link the resolution to this Incident */
+		$_resolution->IncidentId = $this;
+		/* Set the Subject of the Resolution to the supplied Subject, or the Incident Title if none supplied */
+		$_resolution->Subject = ($subject == NULL ? $this->Title : $subject);
+		/* Resolve the Incident */
+		$crmConnector->closeIncident($_resolution, $status);
+	}
 }
 
 ?>
